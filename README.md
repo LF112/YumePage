baa12e8c7d8a1e07146fe6d4a378ed843c4ef5b0
LF112 <lf@lf112.net>
2019-09-27 20:21:24 +0800

    冲冲冲

---
+++ README.md
@@ -0,0 +1,10 @@
+# <h1 align="center">Yume</h1>
+
+><h3 align="center">今天也在绝赞摸鱼中~</h3>
+
+>🎉 This project was produced by LF112 and presented to friends.
+
+### ?
+><h2 align="center">你说 白给 它多香</h>
+
+🎉 Presented to [@yume233](https://github.com/yume233)
---
+++ yume_app.js
@@ -0,0 +1,29 @@
+/*
++----------------------------------------------
+|  _     _____ _ _ ____    _   _ _____ _____ 
+| | |   |  ___/ / |___ \  | \ | | ____|_   _|
+| | |   | |_  | | | __) | |  \| |  _|   | |  
+| | |___|  _| | | |/ __/ _| |\  | |___  | |  
+| |_____|_|   |_|_|_____(_)_| \_|_____| |_|  
+|
+| Do you say now or never Or never too late?
++----------------------------------------------
+| Modified By LF112 [https://www.lf112.net] 
+| on Mar 16th 2019, Presented to @yume_umia
+| Please DO NOT Remove Any Of The Copyright Information.Ribbon
+|
+| Author: LF112
+| ⚡GitHub: [https://github.com/LF112/Yume] ,
+|   Released Under MIT Lisence
++----------------------------------------------
+*/
+window.onload = function() {
+    console.clear();
+    console.log(
+        "\n %c \u26a1futiwolf %c https://www.futiwolf.com %c BY%c LF112  \n\n",
+        "color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;",
+        "background:rgba(197, 197, 197, 0.89); padding:5px 0;",
+        "color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;", "color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;"
+    );
+
+}
