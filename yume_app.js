663b5de510b924be97bd258f8fd5b28a71fd5dda
LF112 <lf@lf112.net>
2019-10-07 12:50:53 +0800

    👌🏻

--- yume_app.js
+++ yume_app.js
@@ -20,7 +20,7 @@
 window.onload = function() {
     console.clear();
     console.log(
-        "\n %c \u26a1futiwolf %c https://www.futiwolf.com %c BY%c LF112  \n\n",
+        "\n %c 😀Akiyama_yume %c https://github.com/LF112/Yume %c BY%c LF112  \n\n",
         "color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;",
         "background:rgba(197, 197, 197, 0.89); padding:5px 0;",
         "color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;", "color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;"
